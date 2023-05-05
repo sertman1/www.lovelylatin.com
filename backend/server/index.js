ff89aa29e87efed39fe50f7d5d23b90afb26b54c
sertman1 <sertman1@jhu.edu>
2023-05-04 22:05:21 -0400

    communication between front and backend; heroku set up

--- backend/server/index.js
+++ backend/server/index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Lovely Latin!");
 });
 
+app.get("/output", (req, res) => {
+  res.send("here is your output...");
+})
+
 app.listen(port, () => {
   console.log(`Express app listening at port: http://localhost:${port}/`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,42 @@
-import { useState } from "react";
+import { useState } from "react"
+import axios from "axios"
 import { TextField, Button, Container } from "@material-ui/core"
+
+const API = "http://localhost:5000/"
 
 function App() {
   const [userInput, setUserInput] = useState("")
 
+  const enter = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axios.get(`${API}`)
+      const data = response.data
+      alert(data)
+    } catch (err) {
+      console.log(err)
+    }
+    return
+  }
 
+  const keyPress = (e) => {
+    if (e.keyCode == 13) {
+      enter()
+    }
+  }
 
   return (
       <Container>
         <h1>The Lovely Latin Language</h1>
-        <TextField fullWidth label="Enter your Latin text here" id="fullWidth" margin="normal"/>
-        <Button variant="contained">Go!</Button>
+        <TextField fullWidth label="Enter your Latin text here" 
+          id="fullWidth" 
+          margin="normal"
+          onKeyDown={keyPress}
+          onChange={(e) => setUserInput(e.target.value)}
+        />
+        <Button variant="contained" onClick={enter}>
+          Go!
+        </Button>
       </Container>
   );
 
