aef9c95b234a08bf3086de2648fb62ec9d7ecc71
sertman1 <sertman1@jhu.edu>
2023-05-04 22:28:12 -0400

    more heroku troubleshooting

--- backend/server/index.js
+++ backend/server/index.js
@@ -6,10 +6,7 @@ app.get("/", (req, res) => {
   res.send("Lovely Latin!");
 });
 
-app.get("/output", (req, res) => {
-  res.send("here is your output...");
-})
-
 app.listen(port, () => {
   console.log(`Express app listening at port: http://localhost:${port}/`);
 });
+
