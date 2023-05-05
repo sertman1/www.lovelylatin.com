bbd6b8f6425a61a01ccef60bbed9cbe35673bf16
sertman1 <sertman1@jhu.edu>
2023-05-05 01:30:04 -0400

    connected backend to python processing script; removed heroku

--- backend/server/index.js
+++ backend/server/index.js
@@ -5,16 +5,38 @@ app.use(cors())
 
 const port = process.env.PORT || 5000;
 
+async function process_text(req,res) {
+  const { userInput } = req.query
+
+  try {
+    var spawn = require("child_process").spawn;
+
+    var executor = spawn('python', ["./latin_processor.py",
+      userInput]);
+
+    executor.stdout.on('data', (data) => {
+      res.send(data)
+    });
+
+    executor.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    executor.on('close', (code) => {
+      console.log(`child process exited with code ${code}`)
+    });
+
+  } catch (err) {
+    console.log(err)
+  }
+}
+
 app.get("/", (req, res) => {
   res.send("Lovely Latin!");
 });
 
-app.get("/output", (req, res) => {
-  const { query } = req.query.userInput
-
-  
-  
-  res.json({ query })
+app.get("/output", (req,res) => {
+  process_text(req,res)
 });
 
 app.listen(port, () => {
